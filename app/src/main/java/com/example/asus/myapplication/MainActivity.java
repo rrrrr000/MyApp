db00181e3b56c1d7044217cc8f857b485a0605a2
ch <13990685003@163.com>
2016-11-27 16:18:09 +0800

    11

---
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.asus.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private BaiduMap map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        map = mMapView.getMap();
+    //开启交通图
+        //map.setTrafficEnabled(true);
+    //普通地图
+        //map.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+    //卫星地图
+        //map.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+
+
+
+    }
+    public void onclick(View view){
+        switch (view.getId()){
+            case R.id.btn:
+                break;
+            case R.id.btnoff:
+
+            break;
+        }
+    };
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
+        
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
+        mMapView.onPause();
+    }
+
+
+}
