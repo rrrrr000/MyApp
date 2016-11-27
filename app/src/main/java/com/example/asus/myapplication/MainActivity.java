94c4bb81f8df0897ee7999a1c1a681aad9d28be8
ch <13990685003@163.com>
2016-11-27 17:40:20 +0800

    123

--- app/src/main/java/com/example/asus/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btnoff:
 
             break;
-        }
+        } //a
     };
     @Override
     protected void onDestroy() {
